f9f9443883843a7f1ab0cd76930c428f771d0ab1
XandraV <veresalexandra95@gmail.com>
2020-11-11 13:14:11 +0000

    added qiskit

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
 from flask import Flask
+import qiskit
+from qiskit import Aer
+from result import get_result
+
 app= Flask(__name__)
 @app.route('/')
 def index():
-  return "<h1>Welcome to CodingX</h1>"
+  q = [{"idx":0, "gates": [{"name":"CX", "param": 1}]}, {"idx":1, "gates": [{"name":"H", "param":""}]}]
+  res = get_result(q)
+  # return "<h1>Welcome to CodingX</h1>"
+  return res
